e2bc866d9a41e3c94724f8708222da86e3a14bb5
jfabian04 <ramosgalindezjerlinsonfabian@gmail.com>
2023-12-05 11:48:10 -0500

    first commit

--- resources/views/users/teachers.blade.php
+++ resources/views/users/teachers.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -77,4 +78,5 @@
             </tbody>
         </table>
     </div>
+    
 @endsection
